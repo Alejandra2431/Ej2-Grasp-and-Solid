e3d1f76c54cc2af8d70ed5ec9a06586012355082
Alejandra2431 <69223047+Alejandra2431@users.noreply.github.com>
2020-10-24 18:19:59 -0300

    cambios recipe

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -27,12 +27,8 @@ namespace Full_GRASP_And_SOLID.Library
 
         public void PrintRecipe()
         {
-            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
-            foreach (Step step in this.steps)
-            {
-                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
-            }
+            ConsolePrinter console= new ConsolePrinter();
+            console.Print(steps, FinalProduct);
         }
     }
 }
